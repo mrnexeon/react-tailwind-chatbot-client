0529ac081650d69aec5575ea77ea8f8d89b59f7a
Andrei Arkhipov <the.nexeon@gmail.com>
2024-07-15 09:02:53 +0200

    Remove redundant SVG element

--- src/App.js
+++ src/App.js
@@ -186,19 +186,6 @@ function App() {
                   clipRule="evenodd"
                 ></path>
               </svg>
-              <svg
-                aria-hidden="true"
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
               <span className="sr-only">Toggle sidebar</span>
             </button>
             <span className="self-center font-semibold whitespace-nowrap dark:text-white">Chatbot</span>
